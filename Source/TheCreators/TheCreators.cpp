73184995f228ae3e943ffafc564f59c646cda37f
caskazz <mm2@taffc.com>
2024-11-05 13:54:58 +0000

    Created Initial Project

---
+++ Source/TheCreators/TheCreators.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TheCreators.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TheCreators, "TheCreators" );
